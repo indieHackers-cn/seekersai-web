3fffeab148cfe3029982979225b07efbf24f990a
GeeMoose <moosemosse19@gmail.com>
2023-10-23 22:48:19 +0800

    删除源码标注区

--- components/ui/Footer/Footer.tsx
+++ components/ui/Footer/Footer.tsx
@@ -97,16 +97,6 @@ export default function Footer() {
             &copy; {new Date().getFullYear()} Seekersai, Inc. All rights reserved.
           </span>
         </div>
-        <div className="flex items-center">
-          <span className="text-white">Crafted by</span>
-          <a href="https://vercel.com" aria-label="Vercel.com Link">
-            <img
-              src="/vercel.svg"
-              alt="Vercel.com Logo"
-              className="inline-block h-6 ml-4 text-white"
-            />
-          </a>
-        </div>
       </div>
     </footer>
   );
