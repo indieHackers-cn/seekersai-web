6758b98bee5be13e719c9a274959fe58d85c3b71
GeeMoose <moosemosse19@gmail.com>
2023-10-23 22:29:31 +0800

    推特字样放大一些

--- components/icons/Twitter.tsx
+++ components/icons/Twitter.tsx
@@ -1,11 +1,11 @@
 const Twitter = ({ ...props }) => {
     return (
       <svg
-        width="48"
-        height="48"
+        width="68"
+        height="68"
         viewBox="0 0 48 48"
         fill="none"
-        xmlns="http://www.w3.org/1999/xlink"
+        xmlns="http://www.w3.org/2000/svg"
       >
         <path
           fillRule="evenodd"
