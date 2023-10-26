c3c089f68aa9d742072169cbcc0b7d419d19cdfc
GeeMoose <moosemosse19@gmail.com>
2023-10-27 01:43:30 +0800

    try again

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
+import { createServerSupabaseClient } from '@/app/supabase-server';
 import SupabaseProvider from './supabase-provider';
 import Footer from '@/components/ui/Footer';
 import Navbar from '@/components/ui/Navbar';
 import { PropsWithChildren } from 'react';
+import { Metadata } from "next";
 import 'styles/main.css';
 
 const meta = {
@@ -10,53 +12,56 @@ const meta = {
   cardImage: '/og.png',
   robots: 'follow, index',
   favicon: '/favicon.ico',
-  url: 'https://subscription-starter.vercel.app',
-  type: 'website'
+  url: 'https://www.seekersai.com'
 };
 
-export const metadata = {
+export const metadata: Metadata = {
   title: meta.title,
   description: meta.description,
-  cardImage: meta.cardImage,
   robots: meta.robots,
-  favicon: meta.favicon,
-  url: meta.url,
-  type: meta.type,
+  icons: {
+    icon: meta.favicon,
+  },
   openGraph: {
     url: meta.url,
     title: meta.title,
     description: meta.description,
-    cardImage: meta.cardImage,
-    type: meta.type,
-    site_name: meta.title
+    images: meta.cardImage,
+    siteName: meta.title,
+    type: 'website'
   },
   twitter: {
     card: 'summary_large_image',
     site: '@vercel',
     title: meta.title,
     description: meta.description,
-    cardImage: meta.cardImage
+    images: meta.cardImage
   }
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   // Layouts must accept a children prop.
   // This will be populated with nested layouts or pages
   children
 }: PropsWithChildren) {
+  const supabase = createServerSupabaseClient();
+  const {
+    data: { user }
+  } = await supabase.auth.getUser();
+
   return (
     <html lang="en">
       <body className="bg-gradient-to-r from-zinc-200 to-cyan-500 loading">
         <SupabaseProvider>
           {/* @ts-expect-error */}
-          <Navbar />
+          { !user && (<Navbar />)}
           <main
             id="skip"
             className="min-h-[calc(100dvh-4rem)] md:min-h[calc(100dvh-5rem)]"
           >
             {children}
           </main>
-          <Footer />
+          { !user && (<Footer />)}
         </SupabaseProvider>
       </body>
     </html>
---
+++ app/workplace/page.tsx
@@ -0,0 +1,7 @@
+import Upload from "./Upload";
+
+export default async function Workplace() {
+    return (
+        <Upload />
+    )
+}
